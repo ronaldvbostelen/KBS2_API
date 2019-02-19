3865f4170c4334f22ab77fbf80b2f502a65effe1
DESKTOP-P03HJ5A\rvanb <rvanbostelen@outlook.com>
2019-02-19 13:27:27 +0100

    push report logic

---
+++ KBS2.WijkagentApp.API/KBS2.WijkagentApp.API/NotificationHub/Hub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.NotificationHubs;
+
+namespace KBS2.WijkagentApp.API.NotificationHub
+{
+    public class Hub : NotificationHubClient
+    {
+        private const string connectionString = "Endpoint=sb://wijkagentnotificationhub.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=3mtEWnCvhbZAIDJmTycAULWZvAsJy2ba4mOVbdMBetU=";
+        private const string hubName = "wijkagentnotificationhub";
+
+        public Hub() : base(connectionString, hubName)
+        {
+            CreateFcmNativeRegistrationAsync("AIzaSyA_3TzcMVgUkA-Wtxth7qrf6W6TJ71VHbA");
+        }
+
+        // create serialised package for processing by app
+        public string CreateMessagePackage(string key, string jsonObject) => $"{{\"data\":{{\"key\":\"{key}\",\"content\":{jsonObject}}}}}";
+    }
+}
